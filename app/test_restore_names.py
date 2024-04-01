86312b9c1c6924b73e1e5fd578cba44546ce99c2
Misha Goryn <mgoryn68@gmail.com>
2024-04-01 14:42:11 +0300

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -4,7 +4,7 @@ from app.restore_names import restore_names
 
 @pytest.mark.parametrize(
     "first_name, last_name, full_name, expected_first_name", [
-        (None, "Holy", "Jack Holy", "Jack"),  # Test when first_name is None
+        (None, "Holy", "Jack Holy", "Jack"),
         ("", "Adams", "Mike Adams", ""),
         ("John", "Doe", "John Doe", "John"),
     ])
@@ -13,11 +13,9 @@ def test_restore_names(
         last_name: str,
         full_name: str,
         expected_first_name: str) -> None:
-    users = \
-        [{
-            "first_name": first_name,
-            "last_name": last_name,
-            "full_name": full_name
-        }]
+    users = [{"first_name": first_name,
+              "last_name": last_name,
+              "full_name": full_name
+              }]
     restore_names(users)
     assert users[0]["first_name"] == expected_first_name
